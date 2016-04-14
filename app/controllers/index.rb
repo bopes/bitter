4429578d46c63a30215dbac778b1cdbb233246bb
Charlie Lee <clcblee@gmail.com>
2016-04-14 15:48:40 -0700

    Add login/out routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,11 @@
 get '/' do
   # Home/splash page
 end
+
+post '/login' do
+  # authenticate user, redirect to personal '/' page
+end
+
+post '/logout' do
+  # sign user out, redirect to public '/'
+end
