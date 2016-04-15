8c12cff6cc0dcd5637d9d4c6740c1bd4b0e1ffeb
Blair White & Charlie Lee <pair+blairwhite.bopes@devbootcamp.com>
2016-04-15 10:47:51 -0700

    Create route skeleton

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,22 @@
 get '/' do
   # Home/splash page
+  if session[:user]
+    @user = session[:user]
+    erb :index_private
+  else
+    erb :index_public
+  end
 end
 
 post '/login' do
   # authenticate user, redirect to personal '/' page
+  # session[:user] = user if user = User.authenticate(params[:username],
+                                                    # params[:password])
+  redirect '/'
 end
 
 post '/logout' do
   # sign user out, redirect to public '/'
+  session.clear
+  redirect '/'
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,5 +1,10 @@
+# before do
+#   redirect '/' unless session[:user]
+# end
+
 get '/users' do
   # Show all users (list of users)
+  @users = User.all
   erb :'/users/index'
 end
 
@@ -10,25 +15,34 @@ end
 
 post '/users' do
   # Save new user, redirect to '/'
-
+  user = User.new(username: params[:username], email: params[:email])
+  user.password = params[:password]
+  user.save
+  redirect '/'
 end
 
 get '/users/:id' do
   # Show individual user page
+  @user = User.find(params[:id])
   erb :'/users/show'
 end
 
 get 'users/:id/edit' do
   # Show edit user page (edit your account details)
+  @user = User.find(params[:id])
   erb :'users/edit'
 end
 
 put 'users/:id' do
   # Save updates to a user, redirect to '/users/:id'
-
+  user = User.find(params:id)
+  user.update(username: params[:username], email: params[:email])
+  redirect "/users/#{session[:user].id}"
 end
 
 delete 'users/:id' do
   # Delete user instance, redirect to all users
-
+  user = User.find(params[:id])
+  user.destroy
+  redirect '/users'
 end
