efc3971a798ed89a50ae0f33ed3b3843e5171687
Blair White <white.blair1@gmail.com>
2016-04-15 15:26:14 -0700

    Add users, beats, and relationships.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,192 @@
+#1
 eric = User.new(username: "Eric", email: "e@e.com")
-
 eric.password = "password"
-
 eric.save
-
+#2
 emmet = User.new(username: "Emmet", email: "g@g.com")
-
 emmet.password = "password"
-
 emmet.save
-
+#3
 charlie = User.new(username: "Charlie", email: "c@c.com")
-
 charlie.password = "password"
-
 charlie.save
-
-charlie = User.new(username: "Charlie", email: "c@c.com")
-
-charlie.password = "password"
-
-charlie.save
-
+#4
+kanye = User.new(username: "KANYE", email: "kanye@kanye.com")
+kanye.password = "kanye"
+kanye.save
+#5
+blair = User.new(username: "Blair", email: "b@b.com")
+blair.password = "blair"
+blair.save
+#6
+kobe = User.new(username: "Kobe", email: "k@k.com")
+kobe.password = "kobe"
+kobe.save
+#7
+steph = User.new(username: "StephCurry", email: "s@s.com")
+steph.password = "steph"
+steph.save
+#8
+jay = User.new(username: "Jay", email: "j@j.com")
+jay.password = "jay"
+jay.save
+#9
+jane = User.new(username: "Jane", email: "jane@jane.com")
+jane.password = "jane"
+jane.save
+#10
+joe = User.new(username: "Joe", email: "joe@joe.com")
+joe.password = "joe"
+joe.save
+#11
+candace = User.new(username: "Candace", email: "candace@candace.com")
+candace.password = "candace"
+candace.save
+#12
+maya = User.new(username: "Maya", email: "m@m.com")
+maya.password = "maya"
+maya.save
+#13
+sue = User.new(username: "Sue", email: "s@s.com")
+sue.password = "sue"
+sue.save
+#14
+neymar = User.new(username: "Neymar", email: "n@n.com")
+neymar.password = "neymar"
+neymar.save
+#15
+messi = User.new(username: "Messi", email: "messi@messi.com")
+messi.password = "messi"
+messi.save
+#16
+swaggyp = User.new(username: "SwaggyP", email: "swaggyp@swaggyp.com")
+swaggyp.password = "swaggyp"
+swaggyp.save
+#17
+draymond = User.new(username: "Draymond", email: "d@d.com"
+draymond.password = "draymond"
+draymond.save
+#18
+alex = User.new(username: "Alex", email: "a@a.com")
+alex.password = "alex"
+alex.save
+#19
+abby = User.new(username: "Abby", email: "ab@ab.com")
+abby.password = "abby"
+abby.save
+#20
+ronda = User.new(username: "Ronda", email: "r@r.com")
+ronda.password = "ronda"
+ronda.save
+################
+Beat.create(content: "whats up bitter?", user_id: 2)
+Beat.create(content: "who needs twitter", user_id: 7)
+Beat.create(content: "I am feeling extra bitter rn", user_id: 1)
+Beat.create(content: "how about this beat though?", user_id: 3)
+Beat.create(content: "I have the freshest beats", user_id: 11)
+Beat.create(content: "beeeeaaaaat", user_id: 3)
 Beat.create(content: "Joe is cool", user_id: 1)
-
-
-Beat.create(content: "eary beat 2", user_id: 3)
-Beat.create(content: "erly beat 2", user_id: 3)
-
-
+Beat.create(content: "CHECK OUT THESE ROUTES", user_id: 9)
+Beat.create(content: "How chill is that pineapple", user_id: 5)
+Beat.create(content: "beat!", user_id: 2)
+Beat.create(content: "whats up bitter???", user_id: 2)
+Beat.create(content: "Friday beats", user_id: 2)
+Beat.create(content: "BITTERRRRRR", user_id: 12)
+Beat.create(content: "Bitter$$$$$$", user_id: 3)
+Beat.create(content: "I AM EXTREMELY BITTER", user_id: 4)
+Beat.create(content: "THIS APP IS $", user_id: 4)
+Beat.create(content: "seriously these routes though...", user_id: 18)
+Beat.create(content: "$$$$$$$", user_id: 4)
+Beat.create(content: "hey kanye", user_id: 1)
+Beat.create(content: "Chunyu was hoopin today", user_id: 16)
+Beat.create(content: "Hello!", user_id: 12)
+Beat.create(content: "It is too hot!", user_id: 14)
+Beat.create(content: "Peter is a scumbag", user_id: 6)
+Beat.create(content: "PINEAPPLE!", user_id: 20)
+Beat.create(content: "Fridayyyyy", user_id: 19)
+Beat.create(content: "(:", user_id: 18)
+Beat.create(content: "beats by dray", user_id: 17)
+Beat.create(content: "whats up", user_id: 16)
+Beat.create(content: "generic beat", user_id: 15)
+Beat.create(content: "angry beat" user_id: 14)
+Beat.create(content: "I AM TIRED AND BITTER", user_id: 6)
+Beat.create(content: "welcome to bitter", user_id: 5)
+Beat.create(content: "orange pineapples", user_id: 5)
+Beat.create(content: "@@@@@@@$$$$$$$$", user_id: 5)
+Beat.create(content: "hello", user_id: 2)
+Beat.create(content: "howdy", user_id: 8)
+Beat.create(content: "!!!!!!!!!", user_id: 9)
+###################
+Relationship.create(followed_id: 1, follower_id: 20)
+Relationship.create(followed_id: 1, follower_id: 19)
+Relationship.create(followed_id: 1, follower_id: 18)
+Relationship.create(followed_id: 1, follower_id: 17)
+Relationship.create(followed_id: 1, follower_id: 16)
+Relationship.create(followed_id: 1, follower_id: 15)
+Relationship.create(followed_id: 1, follower_id: 14)
+Relationship.create(followed_id: 1, follower_id: 12)
+Relationship.create(followed_id: 1, follower_id: 11)
+Relationship.create(followed_id: 2, follower_id: 10)
+Relationship.create(followed_id: 2, follower_id: 9)
+Relationship.create(followed_id: 2, follower_id: 3)
+Relationship.create(followed_id: 2, follower_id: 20)
+Relationship.create(followed_id: 2, follower_id: 19)
+Relationship.create(followed_id: 2, follower_id: 18)
+Relationship.create(followed_id: 2, follower_id: 17)
+Relationship.create(followed_id: 2, follower_id: 16)
+Relationship.create(followed_id: 2, follower_id: 15)
+Relationship.create(followed_id: 2, follower_id: 14)
+Relationship.create(followed_id: 2, follower_id: 13)
+Relationship.create(followed_id: 2, follower_id: 12)
+Relationship.create(followed_id: 2, follower_id: 11)
+Relationship.create(followed_id: 2, follower_id: 10)
+Relationship.create(followed_id: 2, follower_id: 9)
+Relationship.create(followed_id: 4, follower_id: 11)
+Relationship.create(followed_id: 4, follower_id: 12)
+Relationship.create(followed_id: 4, follower_id: 14)
+Relationship.create(followed_id: 4, follower_id: 13)
+Relationship.create(followed_id: 4, follower_id: 15)
+Relationship.create(followed_id: 4, follower_id: 16)
+Relationship.create(followed_id: 4, follower_id: 17)
+Relationship.create(followed_id: 4, follower_id: 18)
+Relationship.create(followed_id: 3, follower_id: 20)
+Relationship.create(followed_id: 3, follower_id: 19)
+Relationship.create(followed_id: 3, follower_id: 18)
+Relationship.create(followed_id: 3, follower_id: 17)
+Relationship.create(followed_id: 5, follower_id: 16)
+Relationship.create(followed_id: 5, follower_id: 20)
+Relationship.create(followed_id: 5, follower_id: 19)
+Relationship.create(followed_id: 5, follower_id: 18)
+Relationship.create(followed_id: 5, follower_id: 17)
+Relationship.create(followed_id: 5, follower_id: 16)
+Relationship.create(followed_id: 5, follower_id: 15)
+Relationship.create(followed_id: 5, follower_id: 14)
+Relationship.create(followed_id: 1, follower_id: 2)
+Relationship.create(followed_id: 1, follower_id: 3)
+Relationship.create(followed_id: 1, follower_id: 4)
+Relationship.create(followed_id: 1, follower_id: 5)
+Relationship.create(followed_id: 1, follower_id: 6)
 Relationship.create(followed_id: 2, follower_id: 1)
+Relationship.create(followed_id: 2, follower_id: 19)
+Relationship.create(followed_id: 2, follower_id: 3)
+Relationship.create(followed_id: 2, follower_id: 4)
+Relationship.create(followed_id: 2, follower_id: 5)
 Relationship.create(followed_id: 3, follower_id: 1)
-Beat.create(content: "Joe is cool", user_id: 1)
-Beat.create(content: "beat 2", user_id: 2)
-Beat.create(content: "beat 2", user_id: 2)
-Beat.create(content: "beat 3", user_id: 2)
-Beat.create(content: "beat 4", user_id: 2)
+Relationship.create(followed_id: 3, follower_id: 2)
+Relationship.create(followed_id: 3, follower_id: 15)
+Relationship.create(followed_id: 3, follower_id: 4)
+Relationship.create(followed_id: 3, follower_id: 5)
+Relationship.create(followed_id: 3, follower_id: 6)
+Relationship.create(followed_id: 4, follower_id: 1)
+Relationship.create(followed_id: 4, follower_id: 2)
+Relationship.create(followed_id: 4, follower_id: 3)
+Relationship.create(followed_id: 4, follower_id: 10)
+Relationship.create(followed_id: 4, follower_id: 5)
+Relationship.create(followed_id: 5, follower_id: 1)
+Relationship.create(followed_id: 5, follower_id: 2)
+Relationship.create(followed_id: 5, follower_id: 3)
+Relationship.create(followed_id: 5, follower_id: 4)
+Relationship.create(followed_id: 5, follower_id: 11)
 
-
-Beat.create(content: "laste beat 3", user_id: 3)
-Beat.create(content: " last beat 4", user_id: 3)
 
 
